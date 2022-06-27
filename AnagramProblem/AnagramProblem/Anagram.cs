af1e29d3c71a404714d9ca87999b22a9219a84da
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-27 09:44:37 +0530

    Check given word same or not

---
+++ AnagramProblem/AnagramProblem/Anagram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramProblem
+{
+    public class Anagram
+    {
+        public bool IsAnagram(string first, string  second)
+        {
+            first = first.ToLower();
+            second = second.ToLower();
+            if(first.Length != second.Length)
+            {
+                Console.WriteLine("Please enter two string as same length.");
+                return false;
+            }
+            else
+            {
+                char[] firstArray = first.ToCharArray();
+                char[] secondArray = second.ToCharArray();
+                Array.Sort(firstArray);
+                Array.Sort(secondArray);
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    if(firstArray[i] != secondArray[i])
+                    {
+                        return false;
+                    }
+                    
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ AnagramProblem/AnagramProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AnagramProblem;
+Console.WriteLine("Welcome to Anagram project!");
+Anagram anagram = new Anagram();
+Console.WriteLine("Find anagram word.");
+Console.Write("Please enter first word: ");
+string first = Console.ReadLine();
+Console.Write("Please enter second word: ");
+string second = Console.ReadLine();
+if (anagram.IsAnagram(first, second))
+{
+    Console.WriteLine("Given word is Anagram");
+
+}
+else
+{
+    Console.WriteLine("Given word is not Anagram");
+}
+
